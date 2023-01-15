b21c6a25c0eee82ce9b1509bdff76362f5258deb
FRuktozza <121963443+FRuktozza@users.noreply.github.com>
2023-01-15 15:07:34 +0300

    Add files via upload

---
+++ Task41/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите количество чисел");
+int m = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+int num;
+
+Console.WriteLine("Введите числа");
+for (int i = 0; i < m; i++)
+{
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num > 0)
+    {
+        counter++;
+    }
+}
+
+Console.WriteLine($"Чисел больше нуля: {counter}");
---
+++ Task43/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите точки b1, k1, b2, k2");
+int[] dots = new int[4];
+
+for (int i = 0; i < 4; i++)
+{
+    dots[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+if (dots[1] == dots[3])
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    double x = (double) (dots[2] - dots[0]) / (dots[1] - dots[3]); // x = (b2 - b1) / (k1 - k2)
+    double y = dots[1] * x + dots[0];
+
+    Console.WriteLine($"({x}, {y})");
+}
